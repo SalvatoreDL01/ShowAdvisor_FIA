29b7e40ef824b12432477c7148ce7da505b4073f
giuseppe <granogiuseppe01@gmail.com>
2023-01-28 12:28:29 +0100

    Aggiunta dei metodi per la traduzione da json a lista di oggetti

--- src/main/java/com/example/showadvisor_fia/HelloServlet.java
+++ src/main/java/com/example/showadvisor_fia/HelloServlet.java
@@ -16,6 +16,7 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        Parser.parse();
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
