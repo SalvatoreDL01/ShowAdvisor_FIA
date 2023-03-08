c33ddbab6139afdd1c5e167d7ab2e7837db050fd
giuseppe <granogiuseppe01@gmail.com>
2023-03-08 19:14:21 +0100

    Creazione nuova funzione di fitness

--- src/main/java/com/example/showadvisor_fia/Individuo.java
+++ src/main/java/com/example/showadvisor_fia/Individuo.java
@@ -11,6 +11,20 @@ import java.util.stream.Stream;
 /*Classe che ha il compito di costruire un individuo */
 public class Individuo extends ArrayList<Show> {
 
+    /* private List<String> generi;
+    private String runtimeDesiderato;
+    private String tipologia;
+    private int max;
+    private int min;
+
+
+    public Fitness(List<String> generi, String runtimeDesiderato, String tipologia,int max,int min) {
+        this.generi = generi;
+        this.runtimeDesiderato = runtimeDesiderato;
+        this.tipologia = tipologia;
+        this.max = max;
+        this.min = min;
+    }*/
     private double fitnessTotale;
     private int nShow;
 
